ada19b418dcf01cbf99c1ef2bb6d25f65aeae6af
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-03-10 17:48:10 +0500

    main_categories

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
