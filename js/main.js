03d077c215756369f338e60d60a5ffec54d9c446
nais2008 <134312352+nais2008@users.noreply.github.com>
2024-03-14 22:39:28 +0500

    js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let vibor_bike = document.getElementById("bike");
+
+vibor_bike.addEventListener("click", function(){
+    let slider = document.querySelector("swiper-container");
+    let image = document.querySelector("#image_bike")
+
+    if (vibor_bike.textContent === "Mountain Bikes"){
+        slider.innerHTML = "<swiper-slide> <img src='img/mountain_bikes1.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes2.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes3.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes4.png' alt=''></swiper-slide>";
+        image.src = "img/mountain_bikes5.png";
+        vibor_bike.style.fontWeight = 900;
+    }
+    if (vibor_bike.textContent === "Road Bikes"){
+        vibor_bike.style.fontWeight = 900;
+        slider.innerHTML = "<swiper-slide> <img src='img/mountain_bikes1.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes2.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes3.png' alt=''></swiper-slide><swiper-slide><img src='img/mountain_bikes4.png' alt=''></swiper-slide>";
+        image.src = "img/mountain_bikes5.png";
+    }
+});
